221bb4660193e5ca2f06d499d95e53dc676ce663
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-11-26 23:10:32 +0630

    Updating needed data to Story Model

--- src/Story/StoryModel.js
+++ src/Story/StoryModel.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose')
 const mongoosePaginate = require('mongoose-paginate-v2')
+const { THROPHY_RANKS, BOOK_CATEGORIES } = require('../../config')
 const { Schema } = mongoose
+const shortUUID = require('short-uuid')
 
 /**
  * Create Story Schema
  * @doc : Story Scalable Schema
  */
 const storySchema = new Schema({
+
+	// Short Url for Sharing Facebook or
+	// For Generate Short URL
+	short_url: {
+		type: String,
+		required: true,
+		index: true,
+		unique: true,
+		default: shortUUID.generate()
+	},
+
+	// Image jus string, path can be defined by controller
+	// sometime we can change space example can use aws storage services
 	image: {
 		type: String,
 		required: true,
@@ -17,6 +32,15 @@ const storySchema = new Schema({
 		type: String,
 		required: true,
 		index: true
+	},
+
+	// Category for Searching
+	category: {
+		type: String,
+		enum: BOOK_CATEGORIES,
+		required: true,
+		index: true,
+		default: 'Horror',
 	},
 
 	// Description is Show Description of Stories
@@ -32,6 +56,8 @@ const storySchema = new Schema({
 	},
 
 	// When Author is publishing story, we can access to edit this story
+	// Note: wanna publish, at least one episode need to be published [set true]
+	// And If Episodes Lenght is zero, we can't published
 	is_published: {
 		type: Boolean,
 		required: true,
@@ -48,11 +74,11 @@ const storySchema = new Schema({
 		index: true,
 	},
 
-	// Throphy, Like Champion, 1stRunnerUP
-	is_has_achievement: {
-		type: Boolean,
-		required: true,
-		default: false,
+	// If User is Premium_Author , we add extra +2 count 5
+	addable_episode_count: {
+		type: Number,
+		requird: true,
+		default: 3,
 		index: true,
 	},
 
@@ -80,6 +106,38 @@ const storySchema = new Schema({
 		index: true,
 	},
 
+	// Follow Each Attributes for Story War
+	// It can be null,
+	story_war: {
+		type: Schema.Types.ObjectId,
+		ref: 'StoryWar',
+		default: null,
+		index: true
+	},
+
+	story_war_position: {
+		type: String,
+		enum: THROPHY_RANKS,
+		default: null,
+		index: true,
+	},
+
+	// Throphy, Like Champion, 1stRunnerUP
+	is_has_achievement: {
+		type: Boolean,
+		required: true,
+		default: false,
+		index: true,
+	},
+
+	// Episodes
+	episodes:	[
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Episode'
+		}
+	],
+
 	// Deleted At
 	deletedAt: {
 		type: Date,
@@ -95,7 +153,7 @@ const storySchema = new Schema({
 // Plugin Paginate
 storySchema.plugin(mongoosePaginate)
 
-const User = mongoose.model('Story', storySchema)
+const Story = mongoose.model('Story', storySchema)
 
-// Export User
-module.exports = User
+// Export Story
+module.exports = Story
