70baa0a35a25fef8a148e369f0567ccf43fd2f7e
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-11-27 23:42:50 +0630

    When update current token we return new updated

--- src/Auth/AuthHelper.js
+++ src/Auth/AuthHelper.js
@@ -3,7 +3,7 @@ const User = require('../User/UserModel')
 module.exports.updateCurrentToken = (userId, token) => {
 	return User.findByIdAndUpdate(userId, {
 		current_token: token
-	})
+	}, { new: true })
 }
 
 
